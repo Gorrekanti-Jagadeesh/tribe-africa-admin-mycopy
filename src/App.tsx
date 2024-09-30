9e06feb71c7ed082ebde5d762ac49e539e3cda1f
sai <sai.kumar.dev1999@gmail.com>
2024-09-30 22:06:16 +0530

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Tribe africa</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
