9240dbb991910073f6c44293889045fcfaf4a669
sai <sai.kumar.dev1999@gmail.com>
2024-09-30 22:21:07 +0530

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        <a href="https://vitejs.dev" target="_blank"></a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
