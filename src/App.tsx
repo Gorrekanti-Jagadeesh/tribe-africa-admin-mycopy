f74bd86c7c9924eccce9f1f32258b3c123cedc48
riyazuddin-1 <riyazuddin9846@gmail.com>
2024-10-11 15:00:49 +0530

    run link: builds issued resolve

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-// import './App.css'
 import HomeScreen from './pages/home/HomeScreen';
 
 function App() {
--- src/pages/home/HomeContainer.tsx
+++ src/pages/home/HomeContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
-import Navbar from '../../molecules/header/HomeHeader';
-import HomePage from '../../molecules/homepage';
-import Footer from '../../molecules/footer/Footer';
+// import React from 'react';
+// import Navbar from '../../molecules/header/HomeHeader';
+// import HomePage from '../../molecules/homepage';
+// import Footer from '../../molecules/footer/Footer';
