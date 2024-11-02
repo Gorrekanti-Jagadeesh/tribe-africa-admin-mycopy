284a408b1cfce9ca8c21add10b6cf1b1f8be51cd
riyazuddin-1 <riyazuddin9846@gmail.com>
2024-11-02 16:22:09 +0530

    blog page banner implementation

--- src/pages/content/blogs/blogs-screen.tsx
+++ src/pages/content/blogs/blogs-screen.tsx
@@ -1,5 +1,6 @@
-// screens/BlogPageScreen.tsx
+import demo from '../../../assets/homepage-welcome-image.png';
 
+import Button from '../../../atoms/custom-button/button';
 import BlogView from '../../../molecules/blogs/blog-view';
 import BlogListing from '../../../molecules/blogs/blog-listing';
 
@@ -14,14 +15,28 @@ interface BlogPageScreenProps {
 
 const BlogPageScreen: React.FC<BlogPageScreenProps> = ({ blogId, blogData }) => {
   return (
-    <div>
+    <div className="p-2 md:p-4 max-w-6xl m-auto">
       {blogId ? (
         <BlogView title={blogData.title} image={blogData.image} content={blogData.content} />
       ) : (
-        <BlogListing
-          heading={'Recent Articles'}
-          data={[]} // Here you could pass real data for recent articles
-        />
+        <div className="">
+          <div id="header" className="flex mb-4">
+            <h1 className="flex-grow border-b-2 border-orange-500 text-4xl max-w-md">Business Articles</h1>
+            <Button className="ms-auto">Get Featured</Button>
+          </div>
+          <div id="banner" className="bg-gray-100 flex justify-center p-12">
+            <img className="aspect-square max-w-80" src={demo} />
+            <div className="flex flex-col gap-4 bg-white p-4 max-h-48 my-auto relative right-4">
+              <h4 className="text-lg font-semibold">Top Businesses in 2024</h4>
+              <div className="overflow-auto flex flex-col gap-2">
+                {[1, 2, 3, 4, 5].map((item) => (
+                  <p>{item}</p>
+                ))}
+              </div>
+            </div>
+          </div>
+          <BlogListing heading={'Recent Articles'} data={[]} />
+        </div>
       )}
     </div>
   );
