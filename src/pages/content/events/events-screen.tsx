586977b74a015ed1139ec70ba754c13e3e02a8d7
riyazuddin-1 <riyazuddin9846@gmail.com>
2024-11-04 18:20:35 +0530

    event card UI changes

--- src/pages/content/events/events-screen.tsx
+++ src/pages/content/events/events-screen.tsx
@@ -29,20 +29,20 @@ const EventsScreen: React.FC<EventsScreenProps> = ({ heading, image, data }) => 
           <UnderlineHeading className="font-bold">{heading}</UnderlineHeading>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 overflow-auto">
             {data.map((item, index) => (
-              <div className="w-full inline-block cursor-pointer" key={index}>
+              <div className="w-full inline-block cursor-pointer rounded-md overflow-hidden border" key={index}>
                 <div
-                  className="aspect-video bg-cover rounded-md group relative overflow-auto"
+                  className="aspect-video bg-cover group relative overflow-auto"
                   style={{
                     backgroundImage: `url(${item.image})`,
                   }}
                 >
-                  <div className="absolute top-0 left-0 right-0 bottom-0 bg-black rounded-md hidden group-hover:flex p-2 items-center justify-center transition-opacity duration-300">
+                  <div className="absolute top-0 left-0 right-0 bottom-0 bg-black hidden group-hover:flex p-2 items-center justify-center transition-opacity duration-300">
                     <div className="text-white">
                       {item.description} {item.description.length}
                     </div>
                   </div>
                 </div>
-                <div>
+                <div className="text-sm flex flex-col gap-2 m-2">
                   <p id="title" className="font-semibold text-lg">
                     {item.title}
                   </p>
