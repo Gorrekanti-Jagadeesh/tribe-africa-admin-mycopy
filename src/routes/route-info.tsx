b9f40927cd2152229b09a91591fd5ca844203b60
Aditya-dnvks <aditya.dnvks@gmail.com>
2024-11-05 13:26:00 +0530

    destiantion deatils screen added

--- src/routes/route-info.tsx
+++ src/routes/route-info.tsx
@@ -3,6 +3,7 @@ import BusinessContainer from '../pages/business/business-container';
 import HolidayContainer from '../pages/holiday/holiday-container';
 import EventsPage from '../pages/content/events/events-container';
 import InnovationsScreen from '../pages/home/navbar-section/discover-section/elements/innovations-screen';
+import DestinationDetailsContainer from '../pages/content/destination-details/details-container';
 
 export interface RouteConfig {
   name: string;
@@ -35,6 +36,11 @@ const appRoutes: RouteConfig[] = [
     name: 'innovations',
     path: '/africa/smart-innovations',
     element: <InnovationsScreen />,
+  },
+  {
+    name: 'destinations',
+    path: '/details/:name',
+    element: <DestinationDetailsContainer />,
   },
 ];
 
