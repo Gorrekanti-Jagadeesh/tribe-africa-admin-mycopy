56ca9afc8e1c5884609f277ae85cccd105b4e36f
riyazuddin-1 <riyazuddin9846@gmail.com>
2024-11-11 11:19:37 +0530

    modal container Responsiveness changes

--- src/molecules/modal/index.tsx
+++ src/molecules/modal/index.tsx
@@ -3,14 +3,15 @@ import React from 'react';
 interface ModalProps {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
+  containerClasses?: string;
   customClasses?: string;
   trigger?: React.ReactNode;
   children: React.ReactNode;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, setIsOpen, trigger, customClasses, children }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, setIsOpen, containerClasses, trigger, customClasses, children }) => {
   return (
-    <div className="m-auto">
+    <div className={`inline-block ${containerClasses}`}>
       {/* Trigger Button */}
       {trigger && (
         <span onClick={() => setIsOpen(true)} className="cursor-pointer">
@@ -26,7 +27,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, setIsOpen, trigger, customClasses
         >
           {/* Modal Container */}
           <div
-            className={`grid relative max-w-5xl mx-auto overflow-hidden ${customClasses}`}
+            className={`grid relative max-w-5xl mx-auto max-h-full overflow-auto ${customClasses}`}
             onClick={(e) => e.stopPropagation()}
           >
             <button
