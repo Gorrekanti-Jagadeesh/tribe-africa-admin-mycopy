6aee1f5735043efa4814a22714cc8652ac4ec710
riyazuddin-1 <riyazuddin9846@gmail.com>
2024-11-13 11:15:57 +0530

    Added list of TODO, for moving further.

--- src/pages/content/qna-form/qna-container.tsx
+++ src/pages/content/qna-form/qna-container.tsx
@@ -3,3 +3,12 @@ import { QNAScreen } from './qna-screen';
 export const QNA = () => {
   return <QNAScreen />;
 };
+
+// TODO:
+/**
+ * 1. Getting user details(user.image and user.name) to show in comment. Currently showing emailID
+ * 2. Formatting the date to DD-MM-YYYY
+ * 3. Description max characters to show, implement a '...see more' to view complete description
+ * 4. Showing questions in the order 'Newest first'
+ * 5. Adding pagination/fetch on scroll, for limiting the number of questions.
+ */
