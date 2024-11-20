947c7cf925533de421721c9e2104285dc92d9cd3
riyazuddin-1 <riyazuddin9846@gmail.com>
2024-11-20 11:59:01 +0530

    alias importing implementation

--- src/pages/home/home-screen.tsx
+++ src/pages/home/home-screen.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
 import { HomeHeader } from '../../molecules/header';
 // Components importing from sections folder
 import HeroSection from './sections/hero-section';
 import Explore from './sections/explore';
 import Experience from './sections/experience';
 import Services from './sections/services';
-// import CharmingHotels from './sections/charming-hotels';
+import CharmingHotels from './sections/charming-hotels';
 import HolidayDestination from './sections/holiday-destination';
 import WorkingRemotely from './sections/working-remotely';
 import BusinessFriendly from './sections/business-friendly';
-import Footer from '../../molecules/footer';
+import Footer from '@molecules/footer';
 
-const HomeScreen: React.FC = () => {
+const HomeScreen = () => {
   return (
     <div className="max-w-screen-2xl m-auto">
       <HomeHeader />
       <div>
         <HeroSection />
         <Explore />
-        {/* <CharmingHotels /> */}
+        <CharmingHotels />
         <HolidayDestination />
         <Experience />
         <WorkingRemotely />
