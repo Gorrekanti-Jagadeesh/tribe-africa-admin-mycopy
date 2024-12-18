de667efb36784bb8dd750a2df6a0a9c2ea903353
Aditya-dnvks <aditya.dnvks@gmail.com>
2024-12-18 18:28:14 +0530

    Integrated sanity to pp institute and changes in events schema

--- src/molecules/navbar/peace-prosperity.tsx
+++ src/molecules/navbar/peace-prosperity.tsx
@@ -1,34 +1,50 @@
 import React from 'react';
 import Button from '../../atoms/custom-button/button';
+import { useQuery } from '@tanstack/react-query';
+import { sanity } from '@utils/sanity';
+import { Loading } from '@atoms/common/loading';
+import { useNavigate } from 'react-router';
 
 const PeaceProsperity: React.FC = () => {
+  const {
+    data: discoverData,
+    error: discoverError,
+    isLoading: discoverLoading,
+  } = useQuery({
+    queryKey: ['peace-prosperity-institute-data'],
+    queryFn: () => sanity.GET(`*[_type == "peace-prosperity-institute"][0]`), // Handle undefined 'country'
+  });
+
+  if (discoverLoading) {
+    return <Loading />;
+  }
+
+  if (discoverError) {
+    return <>Error fetching data..</>;
+  }
+  console.log(discoverData, 'ppppoo');
   return (
     <div className="flex flex-col justify-center p-2 md:p-4">
       <h4 className="text-left text-orange-500 text-lg">&rarr; Peace & Prosperity Institute</h4>
       <div className="flex flex-col md:flex-row my-4">
         <div className="w-full max-w-2xl mb-4 mr-2" style={{ flex: '0 0 40%' }}>
-          <div className="bg-gray-600 h-48 aspect-video w-full flex items-center justify-center rounded-lg">
-            <span className="text-lg">Video</span>
-          </div>
+          <video autoPlay={true} loop={true} muted={true} className="rounded-lg">
+            <source src={discoverData.video} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
         <div style={{ flex: '0 0 60%' }}>
-          <p className="text-start mb-4">
-            The Peace & Prosperity and Think and Grow Rich Institutes have partnered with Tribe Africa to bring this
-            life-changing course to our members across Africa!
-          </p>
-          <p className="text-start mb-4">
-            Based on Napoleon Hill's Think and Grow Rich, whose teachings have inspired millions of leaders worldwide,
-            this course offers practical steps to achieve personal and financial success. By focusing on mental
-            discipline, ambition, and perseverance, you'll transform your mindset and unlock your potential for success.
-          </p>
-          <p className="text-start mb-4">
-            Watch the welcome video from the President of the Peace & Prosperity and Think and Grow Rich Institutes, and
-            gain access to this transformative course!
-          </p>
+          {discoverData.paragraphs.map((each: string) => (
+            <p className="text-start mb-4">{each}</p>
+          ))}
         </div>
       </div>
 
-      <Button className="md:self-end md:w-48">Get the Course</Button>
+      <Button className="md:self-end md:w-48">
+        <a className="md:self-end md:w-48" href={discoverData.coursesUrl} target="_blank" rel="noopener noreferrer">
+          Get the Course
+        </a>
+      </Button>
     </div>
   );
 };
