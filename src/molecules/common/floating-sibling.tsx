7b7809a3bafa1cc07247b6e777f2ceb25cd80759
Aditya Dasari <aditya.dnvks@gmail.com>
2024-12-31 14:32:51 +0530

    Added after work & out and about with sanity

--- src/molecules/common/floating-sibling.tsx
+++ src/molecules/common/floating-sibling.tsx
@@ -1,20 +1,36 @@
 import React, { useState, useRef } from 'react';
 import Button from '../../atoms/custom-button/button';
+import { useNavigate, useParams } from 'react-router';
+import { toKebabCase } from '@utils/common';
 
 interface FloatingSiblingProps {
   component?: React.ReactNode;
   sibling: React.ReactNode;
-  // title?: string;
-  // offset: 'parent' | 'screen';
+  hasSubcategories: boolean;
+  mainCategory: string;
+  country: string;
 }
 
-export const FloatingSibling: React.FC<FloatingSiblingProps> = ({ component, sibling }) => {
-  const [isHovered, setIsHovered] = useState(false);
+export const FloatingSibling: React.FC<FloatingSiblingProps> = ({
+  component,
+  sibling,
+  hasSubcategories,
+  mainCategory,
+  country,
+}) => {
+  const [isClicked, setIsClicked] = useState(false);
   const [positionStyle, setPositionStyle] = useState({});
   const cardRef = useRef<HTMLDivElement>(null);
 
-  const handleMouseEnter = () => {
-    if (cardRef.current && cardRef.current.offsetParent) {
+  const navigation = useNavigate();
+
+  const handleClick = () => {
+    if (!hasSubcategories) {
+      navigation(`/${country}/${toKebabCase(mainCategory)}`);
+      return;
+    }
+
+    if (!isClicked && cardRef.current && cardRef.current.offsetParent) {
       const cardBounds = cardRef.current.getBoundingClientRect();
       const parentBounds = cardRef.current.offsetParent.getBoundingClientRect();
       let style: React.CSSProperties = {};
@@ -34,24 +50,20 @@ export const FloatingSibling: React.FC<FloatingSiblingProps> = ({ component, sib
 
       setPositionStyle(style);
     }
-    setIsHovered(true);
-  };
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
+    setIsClicked(!isClicked); // Toggle the clicked state
   };
 
   return (
-    <div className="relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} ref={cardRef}>
-      <div>{component}</div>
-      {isHovered && (
-        <div
-          className={`absolute z-20 top-0 transition-opacity duration-300 ${isHovered ? 'opacity-100' : 'opacity-0'}`}
-          style={positionStyle}
-        >
+    <div className="relative" ref={cardRef}>
+      <div onClick={handleClick} style={{ cursor: 'pointer' }}>
+        {component}
+      </div>
+      {isClicked && hasSubcategories && (
+        <div className="absolute z-20 top-0 transition-opacity duration-300" style={positionStyle}>
           <Button
             className="absolute flex justify-center items-center -top-2 -right-2 rounded-full w-8 h-8"
-            onClick={handleMouseLeave}
+            onClick={() => setIsClicked(false)} // Close the sibling on button click
           >
             x
           </Button>
@@ -61,3 +73,69 @@ export const FloatingSibling: React.FC<FloatingSiblingProps> = ({ component, sib
     </div>
   );
 };
+
+// Below is for hovering cards..Above is for Clicking cards
+// import React, { useState, useRef } from 'react';
+// import Button from '../../atoms/custom-button/button';
+
+// interface FloatingSiblingProps {
+//   component?: React.ReactNode;
+//   sibling: React.ReactNode;
+//   hasSubcategories: boolean;
+//   // title?: string;
+//   // offset: 'parent' | 'screen';
+// }
+
+// export const FloatingSibling: React.FC<FloatingSiblingProps> = ({ component, sibling, hasSubcategories }) => {
+//   const [isHovered, setIsHovered] = useState(false);
+//   const [positionStyle, setPositionStyle] = useState({});
+//   const cardRef = useRef<HTMLDivElement>(null);
+
+//   const handleMouseEnter = () => {
+//     if (cardRef.current && cardRef.current.offsetParent) {
+//       const cardBounds = cardRef.current.getBoundingClientRect();
+//       const parentBounds = cardRef.current.offsetParent.getBoundingClientRect();
+//       let style: React.CSSProperties = {};
+
+//       // Check available space within the parent container
+//       const rightSpace = parentBounds.right - cardBounds.right;
+//       const leftSpace = cardBounds.left - parentBounds.left;
+
+//       // Determine dropdown positioning based on available space
+//       if (rightSpace >= 200) {
+//         style = { left: '100%', paddingLeft: '8px' };
+//       } else if (leftSpace >= 200) {
+//         style = { right: '100%', paddingRight: '8px' };
+//       } else {
+//         style = { left: '50%', transform: 'translateX(-50%)', top: '100%', marginTop: '8px' };
+//       }
+
+//       setPositionStyle(style);
+//     }
+//     setIsHovered(true);
+//   };
+
+//   const handleMouseLeave = () => {
+//     setIsHovered(false);
+//   };
+
+//   return (
+//     <div className="relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} ref={cardRef}>
+//       <div>{component}</div>
+//       {isHovered && hasSubcategories && (
+//         <div
+//           className={`absolute z-20 top-0 transition-opacity duration-300 ${isHovered ? 'opacity-100' : 'opacity-0'}`}
+//           style={positionStyle}
+//         >
+//           <Button
+//             className="absolute flex justify-center items-center -top-2 -right-2 rounded-full w-8 h-8"
+//             onClick={handleMouseLeave}
+//           >
+//             x
+//           </Button>
+//           {sibling}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
--- src/molecules/layout/nav-floating-layout.tsx
+++ src/molecules/layout/nav-floating-layout.tsx
@@ -1,40 +1,56 @@
 import React from 'react';
 import OverLayCard from '../../atoms/card/overlay-card';
 import { FloatingSibling } from '../common/floating-sibling';
+import { sanityImageUrlBuilder } from '@api/index';
+import { useNavigate, useParams } from 'react-router';
+import { toKebabCase } from '@utils/common';
 
 const NavFloatingLayout: React.FC<{
-  categories: Array<{ title: string; image: string; items: Array<{ title: string }> }>;
-}> = ({ categories }) => {
+  categories: Array<{
+    category: string;
+    icon: [];
+    hasSubcategories: boolean;
+    subcategories: string[];
+  }>;
+  heading: string;
+  country: string;
+}> = ({ categories, heading, country }) => {
+  const navigation = useNavigate();
   return (
     <div>
-      <h4 className="text-left text-orange-500 p-4 max-w-6xl">&rarr; Out & About</h4>
+      <h4 className="text-left text-orange-500 p-4 max-w-6xl">&rarr; {heading}</h4>
       <div className="grid grid-cols-2 md:grid-cols-3 w-full">
-        {categories.map((category, index) => (
+        {categories.map((each, index) => (
           <FloatingSibling
             key={index}
             component={
               <div className="p-4 m-4">
                 <OverLayCard
                   data={{
-                    title: category.title,
-                    image: category.image,
+                    title: each.category,
+                    image: sanityImageUrlBuilder(each.icon).url(),
                   }}
                 />
               </div>
             }
             sibling={
               <div className="min-w-40 h-full md:min-w-64 aspect-square overflow-auto text-left p-4 rounded-lg bg-white text-black">
-                <h4 className="text-orange-500 font-semibold">&rarr; {category.title}</h4>
-                {category.items.map((item, index) => (
-                  <p key={index}>{item.title}</p>
-                ))}
+                <h4 className="text-orange-500 font-semibold">&rarr; {each.category}</h4>
+                {each.subcategories &&
+                  each.subcategories.map((item, index) => (
+                    <p key={index} onClick={() => navigation(`/${country}/${toKebabCase(item)}`)}>
+                      {item}
+                    </p>
+                  ))}
               </div>
             }
+            hasSubcategories={each.hasSubcategories}
+            mainCategory={each.category}
+            country={country}
             // offset="parent"
           />
         ))}
       </div>
-      {/* <FloatingSibling/> */}
     </div>
   );
 };
