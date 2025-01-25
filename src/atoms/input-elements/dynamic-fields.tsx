745c7593373610d12a1051bb6eb3b50c28639960
Aditya Dasari <aditya.dnvks@gmail.com>
2025-01-26 03:35:09 +0530

    Vercel deployment issue

--- src/atoms/input-elements/dynamic-fields.tsx
+++ src/atoms/input-elements/dynamic-fields.tsx
@@ -23,6 +23,7 @@ const FieldSet = ({
   remove: () => void;
   action: (fieldName: string, value: string | number) => void;
 }) => {
+  console.log(action, 'What is the purpose of creatign this?');
   return (
     <div className="flex gap-1">
       {fields.map((field) => (
