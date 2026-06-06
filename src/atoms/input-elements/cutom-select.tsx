95e3e5edc9616d7f36684a4d162fad87b747675a
Jayanth Raj G <jayanthrajgblr@gmail.com>
2026-06-06 21:25:44 +0530

    Enhance AdvertisementForm and CustomSelect components for improved layout and responsiveness

--- src/atoms/input-elements/cutom-select.tsx
+++ src/atoms/input-elements/cutom-select.tsx
@@ -50,8 +50,8 @@ export const CustomSelect = forwardRef<
           onFocus={handleFocus}
           value={value}
           defaultValue={defaultValue}
-          className={`p-2 text-sm block w-1/2 h-10 bg-transparent border outline-none rounded-md focus:border-brand-orange
-          ${error ? 'border-red-500' : 'border-gray-400'}  appearance-none ${customInputClasses}`}
+          className={`p-2 text-sm block w-full h-10 bg-transparent border outline-none rounded-md focus:border-brand-orange
+          ${error ? 'border-red-500' : 'border-gray-400'} appearance-none pr-8 ${customInputClasses}`}
           {...props}
         >
           <option value="" disabled selected={Boolean(!defaultValue)}>
@@ -71,7 +71,7 @@ export const CustomSelect = forwardRef<
           >
             {placeholder}
           </label>
-          <span className="absolute right-[52%] top-10 pointer-events-none text-gray-600 text-xs" aria-hidden="true">
+          <span className="absolute right-3 top-10 pointer-events-none text-gray-600 text-xs" aria-hidden="true">
             ▼
           </span>
         </div>
